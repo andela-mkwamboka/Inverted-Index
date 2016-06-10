43912472935203b8fe1afbc74956fef41f66ae42
andela-mkwamboka <monicah.kwamboka@andela.com>
2016-06-10 10:13:50 +0300

    Reading the Json file: Test suite passing

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -1,22 +1,36 @@
-describe('Read book data', function() {
-  it('Asserts that json file is not empty', function() {
-    expect(jsonData).toBeDefined();
-    expect(typeof jsonData).toEqual('object');
-    expect(jsonData.length).not.toEqual(0);
-
-  });
-});
-describe('Populate Index', function() {
-  it('verifies that the index is created', function() {
-    expect(index.getIndex().length).toBeGreaterThan(0);
-  });
-  it('Ensures index is correct', function() {
-    expect(index.searchIndex('alice')).toEqual([0]);
+describe('Inverted Index Tests', function() {
+  var index = new Index();
+  //Before each runs before each suite
+  beforeEach(function(done) {
+  	// Resolving the promise from the createIndex method 
+  	// Data is the a Javascript object with the Json data
+    index.createIndex('../jasmine/books.json').then(function(data) {
+      index.results = data;
+      done();
+    });
   });
 
-});
-describe('Search index', function() {
-  it('Verifies that search index returns an array ', function() {
+  describe('Read book data', function() {
+    it('Asserts that json file is not empty', function() {
+      var jsonData = index.results;
+      expect(jsonData).toBeDefined();
+      expect(typeof jsonData).toEqual('object');
+      expect(jsonData.length).not.toEqual(0);
+
+    });
+  });
+  describe('Populate Index', function() {
+    it('verifies that the index is created', function() {
+      expect(index.getIndex().length).toBeGreaterThan(0);
+    });
+    it('Ensures index is correct', function() {
+      expect(index.searchIndex('alice')).toEqual([0]);
+    });
+
+  });
+  describe('Search index', function() {
+    it('Verifies that search index returns an array ', function() {
       expect(index.searchIndex([' ', 'alice', 'wonderland', 'lord', 'priest'])).toEqual([0, 0, 1, -1]);
-  })
+    })
+  });
 });
