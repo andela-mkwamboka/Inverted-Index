18039901c6d0f2f47bbf8fc5e5245c7f2d143320
andela-mkwamboka <monicah.kwamboka@andela.com>
2016-06-23 19:04:37 +0300

    CleanUp: removing unnecessary variables

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -1,48 +1,45 @@
 var index = new Index();
 describe('Read book data', function() {
-  it('Asserts that json file is not empty', function(done) {
-    // Resolving the promise from the createIndex method 
-    // Data is the a Javascript object with the Json data
-    index.fetchJson('../jasmine/books.json').then(function(data) {
-      expect(data).toBeDefined();
-      expect(typeof data).toEqual('object');
-      expect(data.length).not.toEqual(0);
-      done();
+    it('Asserts that json file is not empty', function(done) {
+        // Resolving the promise from the createIndex method 
+        // Data is the a Javascript object with the Json data
+        index.fetchJson('../jasmine/books.json').then(function(data) {
+            expect(data).toBeDefined();
+            expect(typeof data).toEqual('object');
+            expect(data.length).toBeGreaterThan(0);
+            done();
+        });
     });
-  });
 });
 
 
 describe('Populate Index', function() {
-  var results = [];
-  beforeEach(function(done) {
-    index.createIndex('../jasmine/books.json').then(function(data) {
-      results = data;
-      done();
+    var results = [];
+    beforeEach(function(done) {
+        index.createIndex('../jasmine/books.json').then(function(data) {
+            results = data;
+            done();
+        });
     });
-  });
 
-  it('verifies that the index is created', function() {
-    expect(results.length).toBeGreaterThan(0);
-  });
-  it('Ensures index is correct', function() {
-    expect(results).toBeDefined();
-    expect(Object.keys(results[0])).toEqual(['alice']);
-    expect(results[0]['alice']).toEqual([0]);
-  });
+    it('verifies that the index is created', function() {
+        expect(results.length).toBeGreaterThan(0);
+    });
+    it('Ensures index is correct', function() {
+        expect(results).toBeDefined();
+        expect(Object.keys(results[0])).toEqual(['alice']);
+        expect(results[0]['alice']).toEqual([0]);
+    });
 
 });
 describe('Search index', function() {
-  it('Verifies that search index returns an array ', function() {
-    expect(index.searchIndex(['123' , ' ' , 'alice' , 'wonderland' , 'lord'])).toEqual([-1, 0, 0, 1]);
-    expect(index.searchIndex('alice wonderland lord')).toEqual([0, 0, 1]);
-    expect(index.searchIndex('people')).toEqual([-1]);
-    expect(index.searchIndex('123')).toEqual([-1]);
-    expect(index.searchIndex()).toEqual(false);
-    expect(index.searchIndex(123)).toEqual(null);
+    it('Verifies that search index returns a correct array ', function() {
+        expect(index.searchIndex(['123', ' ', 'alice', 'wonderland', 'lord'])).toEqual([-1, 0, 0, 1]);
+        expect(index.searchIndex('alice wonderland lord')).toEqual([0, 0, 1]);
+        expect(index.searchIndex('people')).toEqual([-1]);
+        expect(index.searchIndex('123')).toEqual([-1]);
+        expect(index.searchIndex()).toEqual(null);
+        expect(index.searchIndex(123)).toEqual(false);
 
-
-
-    
-  });
+    });
 });
--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,159 +1,155 @@
-function Index () {
-  this.bks = [];
-  this.indexes = {};
-  var self = this;
+function Index() {
+    this.bookArray = [];
+    var self = this;
 
-  var index = [];
-  var stop_words = ['a', 'about', ',', 'above', 'into', 'across', 'of', 'the',
-    'to', 'in', 'and', 'the', 'an'
-  ];
+    var index = [];
+    var stop_words = ['a', 'about', ',', 'above', 'into', 'across', 'of', 'the',
+        'to', 'in', 'and', 'the', 'an', ' ', '.'
+    ];
 
-  this.fetchJson = function (filepath) {
-    // Fetch returns a promise
-    // 1. Takes in a file path
-    // 2. Uses fetch API to request for the json file
-    // 3. Uses Json method to convert result to a javascript object
+    this.fetchJson = function(filepath) {
+        // Fetch returns a promise
+        // 1. Takes in a file path
+        // 2. Uses fetch API to request for the json file
+        // 3. Uses Json method to convert result to a javascript object
 
-    return fetch(filepath)
-      .then(function (response) {
-        // Converting raw data to json
-        if (response.status === 200) {
-          return response.json();
-        } else {
-          throw response;
-        }
-      }).then(function (jsonData) {
-      // Returns a javascript object
-      self.bks = jsonData;
-      return self.bks;
-    }).catch(function (err) {
-      // Throw an error if promise is rejected
-      throw err;
-    });
-  };
+        return fetch(filepath)
+            .then(function(response) {
+                // Converting raw data to json
+                if (response.status === 200) {
+                    return response.json();
+                }
+            }).then(function(jsonData) {
+                // Returns a javascript object
+                self.bookArray = jsonData;
+                return self.bookArray;
+            }).catch(function(err) {
+                // Return error if promise is rejected
+                return err;
+            });
+    };
 
-  this.createIndex = function (filepath) {
-    // Javascript object from the Json file
-    return this.fetchJson(filepath)
-      .then(function (books) {
-        // 1. Looping through the Javascript Object getting the index of each book object
-        // 2. Looping through the book to get book content
-        // 3. Reformat string to lowercase and removing stop words [ Split returns array]
+    this.createIndex = function(filepath) {
+        // Javascript object from the Json file
+        return this.fetchJson(filepath)
+            .then(function(books) {
+                // 1. Looping through the Javascript Object getting the index of each book object
+                // 2. Looping through the book to get book content
+                // 3. Reformat string to lowercase and removing stop words [ Split returns array]
 
-        books.forEach(function (book, i) {
-          for (var key in book) {
-            var bookContent = book[key]
-              .toLowerCase()
-              .split(' ')
-              .filter(function (element) {
-                return stop_words.indexOf(element) === -1;
-              });
-            // Loop through the book content array to get individual word
-            bookContent.forEach(function (word) {
-              var ok = false;
-              // Checking if the index is empty
-              if (index.length) {
-                // Executed on the second loop:
-                // 1. Looping through the index
-                // 2. Checking if word exists
-                // 3. Concatinating index if it is unique
+                books.forEach(function(book, bookIndex) {
+                    for (var key in book) {
+                        var bookContent = book[key]
+                            .toLowerCase()
+                            .split(' ')
+                            .filter(function(element) {
+                                return stop_words.indexOf(element) === -1;
+                            });
+                        // Loop through the book content array to get individual word
+                        bookContent.forEach(function(word) {
+                            var ok = false;
+                            // Checking if the index is empty
+                            if (index.length) {
+                                // Executed on the second loop:
+                                // 1. Looping through the index
+                                // 2. Checking if word exists
+                                // 3. Concatinating index if isn't found
+                                index.forEach(function(obj) {
+                                    if (obj.hasOwnProperty(word)) {
+                                        if (obj[word].indexOf(bookIndex) === -1) {
+                                            obj[word] = obj[word].concat(bookIndex);
+                                        }
+                                        ok = true;
+                                    }
+                                });
+                            }
+                            // Executed on the first loop
+                            if (!ok) {
 
-                index.forEach(function (obj) {
-                  if (obj.hasOwnProperty(word)) {
-                    if (obj[word].indexOf(i) === -1) {
-                      obj[word] = obj[word].concat(i);
+                                index = index.concat({
+                                    // Concat to a new empty Array
+                                    [word]: [].concat(bookIndex)
+                                });
+                            }
+                        });
                     }
-                    ok = true;
-                  }
                 });
-              }
-              // Executed on the first loop
-              if (!ok) {
-                index = index.concat({
-                  // Concat to a new empty Array
-                  [word]: [].concat(i)
+                return index;
+            }).catch(function(error) {
+                return error;
+            });
+    };
+
+    this.getIndex = function() {
+        return index;
+    };
+
+    this.searchIndex = function() {
+        var theIndex = this.getIndex();
+        var results = [];
+        // Checking if arguments have been passed
+        if (arguments.length) {
+            //  Format
+            var format = function(word) {
+                return word.toLowerCase().split(' ').filter(function(element) {
+                    return stop_words.indexOf(element) === -1;
                 });
-              }
+            };
+            // Returning Index of search terms
+            var search = function(oneWord) {
+                found = false;
+                if (theIndex && oneWord) {
+                    theIndex.forEach(function(indexObject) {
+                        if (indexObject.hasOwnProperty(oneWord)) {
+                            found = true;
+                            results = results.concat(indexObject[oneWord]);
+                        }
+                    });
+
+                    if (!found) {
+                        results.push(-1);
+                    }
+                }
+            };
+
+            // 1. Loop through the args to get each term
+            // 2. Check the type of the term
+            // 3. If a string
+            // 4. Split the term in ' '
+            // 5. Loop through the splitted terms
+            // 6. Search for the term
+            // 7. Else if array: 
+            // 8. loop through the array, toLowerCase() search
+
+            var args = Array.from(arguments);
+            args.forEach(function(terms) {
+                if (typeof terms === 'string') {
+                    //  Format word to remove stop word and rswitch to lowercase
+                    var newTerm = format(terms);
+
+                    for (var word in newTerm) {
+                        search(newTerm[word]);
+                    }
+                } else if (Array.isArray(terms)) {
+                    // Loop through the array
+                    terms.forEach(function(element) {
+                        //  Format word to remove stop word and switch to lowercase
+                        element = format(element);
+                        // Search word
+                        element.forEach(function(oneWord) {
+                            search(oneWord);
+                        });
+                    });
+                } else {
+                    // Invalid search term
+                    results = false;
+                }
             });
-          }
-        });
-        self.indexes = index;
-        return self.indexes;
-      }).catch(function (error) {
-      return error;
-    });
-  };
-
-  this.getIndex = function () {
-    return this.indexes;
-  };
-
-  this.searchIndex = function () {
-    var theIndex = this.getIndex();
-    var results = [];
-    // Checking if arguments have been passed
-    if (arguments.length) {
-      //  Format
-      var format = function (word) {
-        return word.toLowerCase().split(' ').filter(function (element) {
-          return stop_words.indexOf(element) === -1;
-        });
-      };
-      // Returning Index of search terms
-      var search = function (oneWord) {
-        found = false;
-        if (theIndex && oneWord) {
-          theIndex.forEach(function (value) {
-            if (value.hasOwnProperty(oneWord)) {
-              found = true;
-              results = results.concat(value[oneWord]);
-            }
-          });
-          if (!found) {
-            results.push(-1);
-          }
-        }
-      };
-
-      // 1. Loop through the args to get each term
-      // 2. Check the type of the term
-      // 3. If a string
-      // 4. Split the term in ' '
-      // 5. Loop through the splitted terms
-      // 6. Search for the term
-      // 7. Else if array: 
-      // 8. loop through the array, toLowerCase() search
-
-      var args = Array.from(arguments);
-      args.forEach(function (terms) {
-        if (typeof terms === 'string') {
-          //  Format word to remove stop word and rswitch to lowercase
-          var newTerm = format(terms);
-
-          for (var word in newTerm) {
-            var findWord = newTerm[word].replace(/[.,?\s]/g, '');
-            search(findWord);
-          }
-        } else if (Array.isArray(terms)) {
-          // Loop through the array
-          terms.forEach(function (element) {
-            //  Format word to remove stop word and switch to lowercase
-            element = format(element);
-            // Search word
-            element.forEach(function (oneWord) {
-              search(oneWord);
-            });
-          });
+            console.log(results);
+            return results;
         } else {
-          // Invalid search term
-          results = null;
+            // You need to search for a term
+            return null;
         }
-      });
-      console.log(results);
-      return results;
-    } else {
-      // You need to search for a term
-      return false;
-    }
-  };
+    };
 }
